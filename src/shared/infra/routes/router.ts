a4b7b52feba40a0626ae11695f93e3a5c04cbc03
luizaoformigapqd <luizlima@garantti.com.br>
2022-05-21 17:36:24 -0300

    refactor: :fire: Fazendo alterações nos arquivos (exclusão)

--- src/shared/infra/routes/router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { usersRouter } from "modules/users/http/infra/routes/usersRouter";
-
-const router = Router();
-
-router.use("/users", usersRouter);
-
-export { router };
--- src/shared/infra/server.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "./app";
-
-const port = 5000;
-
-app.listen(port, () => {
-  console.log(`Server running to port http://localhost:5000`);
-});
